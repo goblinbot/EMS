4146a86562f2a6589f5c9d618e22a3ae64556fed
Goldberg <joshgoldberg@quickenloans.com>
2019-03-13 14:38:55 -0400

    Add ICDate and ICTime

--- _includes/date.php
+++ _includes/date.php
@@ -9,4 +9,6 @@ $dt->setTimestamp($timestamp); //adjust the object to correct timestamp
 $json = file_get_contents('http://localhost:5000/api/time');
 $obj = json_decode($json);
 $DateTime = $obj->iDay . "-" . $obj->iMonth . "-" . $obj->iYear . $obj->iYearAfter . " " .  $dt->format('H:i:s') . "\n";;
+$ICDate = $obj->iDay . "-" . $obj->iMonth . "-" . $obj->iYear . $obj->iYearAfter;
+$ICTime =  $dt->format('H:i:s') . "\n";
 ?>
