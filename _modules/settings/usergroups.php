3516e1156a4a8f08579278f30e14b5df8e6c780b
Goldberg <joshgoldberg@quickenloans.com>
2019-03-15 14:33:11 -0400

    Update usergroups.php

--- _modules/settings/usergroups.php
+++ _modules/settings/usergroups.php
@@ -66,7 +66,7 @@ if(!isset($_SESSION)) {
 
       $printbox = "";
 
-       /*foreach($userArr AS $TITLE => $CATEGORIE) {
+       foreach($userArr AS $TITLE => $CATEGORIE) {
 
         if($printbox === "") {
           $class = "tab on";
@@ -75,10 +75,10 @@ if(!isset($_SESSION)) {
         }
 
         // tabjes maken voor top
-        $printresult .= "<a onclick=\"activateTab('".$TITLE."');\" class=\"button button-tab\"><i class=\"fa fa-angle-down\"></i>&nbsp;".$TITLE."</a>";
+        //$printresult .= "<a onclick=\"activateTab('".$TITLE."');\" class=\"button button-tab\"><i class=\"fa fa-angle-down\"></i>&nbsp;".$TITLE."</a>";
 
         // de tables bouwen.
-        $printbox .= "<div id=\"tab_".$TITLE."\" class=\"ammobox $class\">"; */
+        // $printbox .= "<div id=\"tab_".$TITLE."\" class=\"ammobox $class\">"; 
         // $printresult .= "<h3 class=\"\">".$TITLE."</h3>";
         $printbox .= "<table class=\"table\">";
         $printbox .= "<thead>"
