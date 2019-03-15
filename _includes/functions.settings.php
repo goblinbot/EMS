aaeefabf0d11fac727c3a48598eea24a454536b5
Goldberg <joshgoldberg@quickenloans.com>
2019-03-15 14:17:59 -0400

    Update functions.settings.php

--- _includes/functions.settings.php
+++ _includes/functions.settings.php
@@ -5,7 +5,7 @@ function ar_initUsergroups() {
   $userArr = array();
 
   // haalt de gebruikersgroepstoewijzingen op
-  $sql = "SELECT id,group_id,user_id FROM user_koppeling ORDER BY id ASC";
+  $sql = "SELECT user_koppeling.id,user_koppeling.group_id,user_koppeling.user_id,t1.fullname FROM user_koppeling join users t1 on t1.id = user_koppeling.user_id ORDER by user_koppeling.id ASC";
   $result = $UPLINK->query($sql);
 
   // loopt door de results heen
@@ -15,13 +15,13 @@ function ar_initUsergroups() {
 
     while($row = mysqli_fetch_assoc($result)){
 
-      if(isset($row['group_id']) && $row['group_id'] != "") {
+      //if(isset($row['group_id']) && $row['group_id'] != "") {
         foreach($row AS $KEY => $VALUE) {
           $VALUE = strtolower($VALUE);
           // VOLLEDIGE DATA ->CATEGORIE ->UNIEKE ID ->ALLE WAARDES
           $userArr[strtolower($row['group_id'])][$row['id']][$KEY] = EMS_echo($VALUE);
 
-        }//foreach
+        //}//foreach
       } /// if != ""
 
     } //while results
